9d3252583836d4f74aa00ef3fcb7e27953ada347
Tesh Patel <tesh.p@hotmail.co.uk>
2022-10-04 21:04:24 +0100

    Descripton button feature added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // add event listener to all buttons
+let allAboutBtns = document.getElementsByClassName("btn-about");
+let allDescriptionTexts = document.getElementsByClassName("desc-text");
+let testBox = document.getElementsByClassName("desc-boxes");
 
-// add unique id to all buttons
+for (let i = 0; i < allAboutBtns.length; i++) {
+  allAboutBtns[i].addEventListener("click", activeText);
+}
 
-// change class from hidden to active
+// aboutBtn.addEventListener("click", activeText);
 
-function activeText() {
-  console.log("Btn working");
+// 1. get all btns
+// 2. get all boxes
+
+function activeText(projectNumber) {
+  console.log(this.value);
+  testBox[this.value].classList.toggle("desc-active");
+
+  //   console.log(this.value);
+  //   allDescriptionTexts[this.value].classList.toggle("desc-active");
+  // get value of THIS btn
+  // activate class on for [this] text
 }
