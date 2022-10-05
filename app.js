4f84545b1d621dec92cf647b95ebc9371a83acd0
Tesh Patel <tesh.p@hotmail.co.uk>
2022-10-05 16:36:03 +0100

    Adjustable gallery-container to fit screen size

--- app.js
+++ app.js
@@ -7,17 +7,7 @@ for (let i = 0; i < allAboutBtns.length; i++) {
   allAboutBtns[i].addEventListener("click", activeText);
 }
 
-// aboutBtn.addEventListener("click", activeText);
-
-// 1. get all btns
-// 2. get all boxes
-
 function activeText(projectNumber) {
   console.log(this.value);
   testBox[this.value].classList.toggle("desc-active");
-
-  //   console.log(this.value);
-  //   allDescriptionTexts[this.value].classList.toggle("desc-active");
-  // get value of THIS btn
-  // activate class on for [this] text
 }
